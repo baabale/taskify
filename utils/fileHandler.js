310b802ae1ac4732b3af191e41ad83f72101d7cd
Abdirahman Baabale <baabale@gmail.com>
2024-10-27 07:45:34 +0300

    first commit

---
+++ utils/fileHandler.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = './data/tasks.json';
+
+exports.writeTasksToFile = (tasks) => {
+    fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2))
+}
+
+exports.readTasksFromFile = () => {
+    if(!fs.existsSync(filePath)) {
+        this.writeTasksToFile([])
+    }
+
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data)
+}
